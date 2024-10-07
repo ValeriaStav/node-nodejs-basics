b042bfbafec936cb8a8bb294b54264466c3d2107
ValeriaStav <ronnyramb@gmail.com>
2024-10-07 23:40:18 +0300

    feat: implement Worker Threads (src/wt) tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { Worker, isMainThread } from "worker_threads";
+import { cpus } from "node:os";
+
 const performCalculations = async () => {
-    // Write your code here
+  if (!isMainThread) return;
+
+  const numCPUs = cpus().length;
+  const results = [];
+
+  const promises = Array.from({ length: numCPUs }, (_, i) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(new URL("./worker.js", import.meta.url), {
+        workerData: 10 + i
+      });
+
+      worker.on("message", (result) => {
+        results.push({ status: "resolved", data: result });
+        resolve();
+      });
+
+      worker.on("error", () => {
+        results.push({ status: "error", data: null });
+        resolve();
+      });
+    });
+  });
+
+  await Promise.all(promises);
+  console.log(results);
 };
 
 await performCalculations();
