a187c3c3e273ed817ce3f98c6e52e23112cb8eab
ValeriaStav <ronnyramb@gmail.com>
2024-10-07 23:08:50 +0300

    feat: implement Zlib (src/zip) tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream/promises";
+
 const decompress = async () => {
-    // Write your code here 
+  const source = createReadStream("src/zip/files/archive.gz");
+  const destination = createWriteStream("src/zip/files/fileToCompress.txt");
+  const gunzip = createGunzip();
+
+  pipeline(source, gunzip, destination);
 };
 
 await decompress();
