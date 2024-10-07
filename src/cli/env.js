8e05b408afb5bf146e76806c52a035b5f5d977cd
ValeriaStav <ronnyramb@gmail.com>
2024-10-07 18:37:44 +0300

    feat: implement Command line interface(src/cli) tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVars = [];
+  for (const key in process.env) {
+    if (key.startsWith("RSS_")) {
+      envVars.push(`${key}=${process.env[key]}`);
+    }
+  }
+  console.log(envVars.join("; "));
 };
 
 parseEnv();
